e4e301d3307f8ca898f15ec532fc4961659378e4
gabrielsousaf <gs294860@gmail.com>
2023-01-08 20:23:27 -0300

    final commit

---
+++ src/js/key.js
@@ -0,0 +1,7 @@
+// key = "228cfefac5b85af9fc76719bc59ef9cd"
+const api = {
+    key: "228cfefac5b85af9fc76719bc59ef9cd",
+    base : "https://api.openweathermap.org/data/2.5/",
+    lang: "pt_br",
+    units: "metric"
+}
